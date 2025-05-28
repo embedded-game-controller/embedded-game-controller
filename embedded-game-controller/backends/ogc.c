3ac82790a4b02bf63c80e9f76b2d751c409211e2
Sergi Granell <xerpi.g.12@gmail.com>
2025-05-28 23:09:53 +0900

    egc_types.h: include inttypes.h

--- embedded-game-controller/backends/ogc.c
+++ embedded-game-controller/backends/ogc.c
@@ -408,7 +408,7 @@ static void handle_device_change_reply(int host_fd, areply *reply)
     int ret;
     bool found;
 
-    LOG_DEBUG("Device change, #Attached devices: %d\n", reply->result);
+    LOG_DEBUG("Device change, #Attached devices: %" PRId32 "\n", reply->result);
 
     if (reply->result < 0)
         return;
@@ -430,7 +430,8 @@ static void handle_device_change_reply(int host_fd, areply *reply)
 
         /* Oops, it got disconnected */
         if (!found) {
-            LOG_DEBUG("Device with VID: 0x%04x, PID: 0x%04x, dev_id: 0x%x got disconnected\n",
+            LOG_DEBUG("Device with VID: 0x%04" PRIx16 ", PID: 0x%04" PRIx16 ", dev_id: 0x%" PRIx32
+                      " got disconnected\n",
                       device->usb.vid, device->usb.pid, device->usb.dev_id);
 
             if (!queue_event(EGC_EVENT_DEVICE_REMOVED, device)) {
@@ -448,7 +449,8 @@ static void handle_device_change_reply(int host_fd, areply *reply)
         vid = device_change_devices[i].vid;
         pid = device_change_devices[i].pid;
         dev_id = device_change_devices[i].device_id;
-        LOG_DEBUG("[%d] VID: 0x%04x, PID: 0x%04x, dev_id: 0x%x\n", i, vid, pid, dev_id);
+        LOG_DEBUG("[%d] VID: 0x%04" PRIx16 ", PID: 0x%04" PRIx16 ", dev_id: 0x%" PRIx32 "\n", i,
+                  vid, pid, dev_id);
 
         /* Check if we already have that device (same dev_id) connected */
         if (is_usb_device_connected(dev_id))
