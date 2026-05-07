661f4350ff05c19489d338331c9111e0754254ae
Alberto Mardegan <mardy@users.sourceforge.net>
2026-05-08 00:03:44 +0300

    generic driver: remove unused static variable

--- embedded-game-controller/usb_drivers/generic.c
+++ embedded-game-controller/usb_drivers/generic.c
@@ -9,40 +9,6 @@ struct dr_private_data_t {
 static_assert(sizeof(struct dr_private_data_t) <= EGC_INPUT_DEVICE_DRIVER_DATA_SIZE);
 #define PRIV(input_device) ((struct dr_private_data_t *)get_priv(input_device)->private_data)
 
-static const egc_device_description_t s_device_description = {
-    .vendor_id = 0x0079,
-    .product_id = 0x0006,
-    /* clang-format off */
-    .available_buttons =
-        BIT(EGC_GAMEPAD_BUTTON_DPAD_UP) |
-        BIT(EGC_GAMEPAD_BUTTON_DPAD_DOWN) |
-        BIT(EGC_GAMEPAD_BUTTON_DPAD_LEFT) |
-        BIT(EGC_GAMEPAD_BUTTON_DPAD_RIGHT) |
-        BIT(EGC_GAMEPAD_BUTTON_NORTH) |
-        BIT(EGC_GAMEPAD_BUTTON_EAST) |
-        BIT(EGC_GAMEPAD_BUTTON_SOUTH) |
-        BIT(EGC_GAMEPAD_BUTTON_WEST) |
-        BIT(EGC_GAMEPAD_BUTTON_LEFT_SHOULDER) |
-        BIT(EGC_GAMEPAD_BUTTON_RIGHT_SHOULDER) |
-        BIT(EGC_GAMEPAD_BUTTON_BACK) |
-        BIT(EGC_GAMEPAD_BUTTON_START) |
-        BIT(EGC_GAMEPAD_BUTTON_LEFT_STICK) |
-        BIT(EGC_GAMEPAD_BUTTON_RIGHT_STICK),
-    .available_axes =
-        BIT(EGC_GAMEPAD_AXIS_LEFTX) |
-        BIT(EGC_GAMEPAD_AXIS_LEFTY) |
-        BIT(EGC_GAMEPAD_AXIS_RIGHTX) |
-        BIT(EGC_GAMEPAD_AXIS_RIGHTY) |
-        BIT(EGC_GAMEPAD_AXIS_LEFT_TRIGGER) |
-        BIT(EGC_GAMEPAD_AXIS_RIGHT_TRIGGER),
-    /* clang-format on */
-    .type = EGC_DEVICE_TYPE_GAMEPAD,
-    .num_touch_points = 0,
-    .num_leds = 0,
-    .num_accelerometers = 0,
-    .has_rumble = false,
-};
-
 static void dr_driver_ops_intr_event(egc_input_device_t *device, const void *data, u16 length)
 {
     struct dr_private_data_t *priv = PRIV(device);
